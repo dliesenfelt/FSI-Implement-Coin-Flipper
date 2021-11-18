ad65d66d67c1d141373d8c9c04838d4d35dba673
dliesenfelt <liesenfelt.code@gmail.com>
2021-11-18 09:06:10 -0800

    coin flipper assement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,52 @@
-// TODO: Declare any global variables we need
-
+//Varriables
+let totalHeads = 0;
+let totalTails = 0;
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
+    // The click button that starts everything 
+    document.getElementById('flip').addEventListener('click', () => {
+        let heads = Math.random() < 0.5
+        //if the outcome of math.random is < 0.5 the coin is heads and displays heads image...
+        if (heads == true) {
+            document.getElementById('penny-image').src = 'assets/images/penny-heads.jpg'
+            document.getElementById('message').textContent = "The Coin Is Heads!"
+            totalHeads += 1
+        }
+        // but if it is > 0.5 the coin will be tails and will display tails image/message instead
+        else if (heads == false) {
+            document.getElementById('penny-image').src = 'assets/images/penny-tails.jpg'
+            document.getElementById('message').textContent = "The Coin Is Tails!"
+            totalTails += 1
+        }
+        //Now we get the varriables for total flips weve done...
+        let totalFlips = totalTails + totalHeads;
+        let percentHeads = 0;
+        let percentTails = 0;
+        //and calculate the %
+        if (totalFlips > 0) {
+            percentHeads = Math.round((totalHeads / totalFlips) * 100)
+            percentTails = Math.round((totalTails / totalFlips) * 100)  
+        }
+        //Update the scoreboard with all the values
+        document.getElementById('heads').textContent = totalHeads
+        document.getElementById('tails').textContent = totalTails
+        document.getElementById('heads-percent').textContent = percentHeads
+        document.getElementById('tails-percent').textContent = percentTails
+        console.log(heads,totalTails,totalHeads,totalFlips,percentHeads,percentTails)
+    })
+    // wipes the scoreboard
+    document.getElementById('clear').addEventListener('click', function () {
+        totalHeads = 0
+        totalTails = 0
+        totalFlips = 0
+        percentHeads = 0
+        percentTails = 0
+        document.getElementById('heads').textContent = totalHeads
+        document.getElementById('tails').textContent = totalTails
+        document.getElementById('heads-percent').textContent = percentHeads
+        document.getElementById('tails-percent').textContent = percentTails
+        document.getElementById('message').textContent = "Flip The Coin!"
+        document.getElementById('penny-image').src = 'assets/images/penny-heads.jpg'
+        console.log('Cleared')
+    })    
 })
